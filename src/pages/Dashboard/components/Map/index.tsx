c8e41ad43308b4f47358199494f8fa98b8042ac9
chris023 <chris.meyer023@gmail.com>
2022-03-07 17:06:03 -0700

    Set map center to Denver

--- src/pages/Dashboard/components/Map/index.tsx
+++ src/pages/Dashboard/components/Map/index.tsx
@@ -1,23 +1,23 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-import { Box } from "@tracker/common"
+import { Box } from "@tracker/common";
 
 const Map = () => {
   return (
     <Box sx={{ position: "relative", height: `calc(100vh - 64px)` }}>
-      <MapContainer center={[51.505, -0.09]} zoom={13}>
+      <MapContainer center={[39.7392, -104.9903]} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[39.7392, -104.9903]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
       </MapContainer>
     </Box>
-  )
-}
+  );
+};
 
-export { Map }
+export { Map };
